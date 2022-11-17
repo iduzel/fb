52956c2e09c5c168a8a53747540ddc3d95733393
iduzel <iduzel49@gmail.com>
2022-11-18 00:13:21 +0100

    bs9

--- src/firebase.js
+++ src/firebase.js
@@ -22,12 +22,12 @@ import { login as loginHandle, logout as logoutHandle } from "./store/auth";
 import { openModal } from "./store/modal";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_DIESEL_FB_API_KEY, 
-  authDomain: process.env.REACT_APP_DIESEL_FB_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_DIESEL_FB_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_DIESEL_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_DIESEL_FB_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_DIESEL_FB_APP_ID,
+  apiKey: `${process.env.REACT_APP_DIESEL_FB_API_KEY}`, 
+  authDomain: `${process.env.REACT_APP_DIESEL_FB_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_DIESEL_FB_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_DIESEL_FB_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_DIESEL_FB_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_DIESEL_FB_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
