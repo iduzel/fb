024a85ea783a82fae01d6464dbdc3510d7e788c4
Ismail Duzel <iduzel49@gmail.com>
2022-11-21 00:17:17 +0100

    bs12

--- src/firebase.js
+++ src/firebase.js
@@ -22,12 +22,13 @@ import { login as loginHandle, logout as logoutHandle } from "./store/auth";
 import { openModal } from "./store/modal";
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_DIESEL_FB_API_KEY}`, 
-  authDomain: `${process.env.REACT_APP_DIESEL_FB_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_DIESEL_FB_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_DIESEL_FB_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_DIESEL_FB_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_DIESEL_FB_APP_ID}`,
+    /*apiKey: process.env.REACT_APP_API_KEY, ,*/ 
+  apiKey: 'AIzaSyC4vt4q8fNqtUu_Q67kcV9ccGxYrmgRi7k',
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
