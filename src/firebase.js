7a1506a32fb3da1cbc6dd10bac5a42c9dc9f0261
Ismail Duzel <iduzel49@gmail.com>
2022-11-21 14:14:44 +0100

    dotenv updates

--- src/firebase.js
+++ src/firebase.js
@@ -22,8 +22,7 @@ import { login as loginHandle, logout as logoutHandle } from "./store/auth";
 import { openModal } from "./store/modal";
 
 const firebaseConfig = {
-    /*apiKey: process.env.REACT_APP_API_KEY, ,*/ 
-  apiKey: 'AIzaSyC4vt4q8fNqtUu_Q67kcV9ccGxYrmgRi7k',
+  apiKey: process.env.REACT_APP_API_KEY,  
   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
